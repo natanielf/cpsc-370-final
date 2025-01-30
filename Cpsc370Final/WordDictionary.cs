5efbc688d3d236d5962b35cad14949c561a6db8f
Nataniel Farzan <natanielfarzan@gmail.com>
2025-01-29 19:30:14 -0800

    Update word dictionary categories

--- Cpsc370Final/WordDictionary.cs
+++ Cpsc370Final/WordDictionary.cs
@@ -3,10 +3,11 @@ namespace Cpsc370Final;
 public class WordDictionary
 {
     public List<string> Adjectives { get; set; }
-    public List<string> Nouns { get; set; }
+    public List<string> Persons { get; set; }
+    public List<string> Places { get; set; }
+    public List<string> Objects { get; set; }
     public List<string> Names { get; set; }
     public List<string> Verbs { get; set; }
     public List<string> Adverbs { get; set; }
     public List<string> CussWords { get; set; }
-    public List<string> BrainRot { get; set; }
 }
