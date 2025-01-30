12517c57a2b67df1cf75bf5368fe50d3180833fb
Nataniel Farzan <natanielfarzan@gmail.com>
2025-01-29 20:31:41 -0800

    Print mad lib to terminal

--- Cpsc370Final/Program.cs
+++ Cpsc370Final/Program.cs
@@ -25,9 +25,15 @@ class Program
         }
 
         StoryParser storyParser = new StoryParser(StoryFileName);
-        storyParser.ParseStoryFile();
+        string[] storyTemplate = storyParser.ParseStoryFile();
         WordParser wordParser = new WordParser(DictionaryFileName);
         WordDictionary wordDictionary = wordParser.GetWordDictionary();
+
+        MadLib madLib = new MadLib(storyTemplate, wordDictionary);
+        madLib.GenerateFinalStory(); 
+        string story = madLib.GetFinalStory();
+
+        Console.WriteLine(story);
 
         // you can delete this if/when you like
         //ShowArguments(args);
