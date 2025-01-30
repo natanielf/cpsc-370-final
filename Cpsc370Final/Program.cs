ceb52fb74031b6572d9678acefcab88a8a4da18c
Halle Broadnax <broadnaxhalle@gmail.com>
2025-01-29 17:02:12 -0800

    Added ParseStoryFile() and Updated Program.cs
    ParseStoryFile() takes in the file name, reads it into a string, then splits the string by spaces. It also currently prints out the parsed string.
    Changed Program.cs to test StoryParser.

--- Cpsc370Final/Program.cs
+++ Cpsc370Final/Program.cs
@@ -1,22 +1,18 @@
 ﻿namespace Cpsc370Final;
-
 class Program
 {
-    private static string StoryFileName;
-    private static string DictionaryFileName;
+    private static string StoryFileName = "DefaultStory.txt";
+    private static string DictionaryFileName = "DefaultWords.json";
     static void Main(string[] args)
     {
         if (args.Length < 1)
         {
             Console.WriteLine("Using default story and dictionary.");
-            StoryFileName = "DefaultStory.txt";
-            DictionaryFileName = "DefaultWords.json";
         }
         else if (args.Length == 1)
         {
             Console.WriteLine("Using default dictionary.");
             StoryFileName = args[0];
-            DictionaryFileName = "DefaultWords.json";
         }
         else if (args.Length == 2)
         {
@@ -26,13 +22,14 @@ class Program
         else
         {
             Console.WriteLine("Too many arguments. Using default story and dictionary.");
-            StoryFileName = "DefaultStory.txt";
-            DictionaryFileName = "DefaultWords.json";
         }
-            
+
+        StoryParser sp = new StoryParser(StoryFileName);
+        
+        sp.ParseStoryFile();
         
         // you can delete this if/when you like
-        ShowArguments(args);
+        //ShowArguments(args);
     }
 
     // this is just an example of how to get the command
--- Cpsc370Final/StoryParser.cs
+++ Cpsc370Final/StoryParser.cs
@@ -4,19 +4,26 @@ public class StoryParser
 {
     private string _fileName;
 
-    private List<string> _story;
     //constructor given a file name
     public StoryParser(string fileName)
     {
         _fileName = fileName;
-        _story = new List<string>();
     }
-
     
-    public List<string> ParseStoryFile()
+    public string[] ParseStoryFile()
     {
-        List<string> parsedStory = new List<string>();
-
-        return parsedStory;
+        //read the file and save it all to a string
+        string storyText = File.ReadAllText(_fileName);
+        
+        //split the string into an array of strings
+        string[] storyArray = storyText.Split(" ");
+        
+        for (int i = 0; i < storyArray.Length; i++)
+        {
+            Console.WriteLine(storyArray[i]);
+        }
+        return storyArray;
     }
+    
+    
 }
